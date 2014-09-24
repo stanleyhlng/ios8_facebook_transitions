7b4201825bac42e2ada79c1f2f493d1a4dfca576
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-24 16:41:07 -0700

    Initial Commit

---
+++ ios8_facebook_transitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios8_facebook_transitions
+//
+//  Created by Stanley Ng on 9/24/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
