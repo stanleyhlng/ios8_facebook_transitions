f422041a55b6eaf47e1552d97c2320e01906dd00
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-24 17:33:15 -0700

    config images

---
+++ ios8_facebook_transitions/FeedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FeedViewController.swift
+//  ios8_facebook_transitions
+//
+//  Created by Stanley Ng on 9/24/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        println("FeedViewController.viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
